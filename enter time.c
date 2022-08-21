53420cd47ea3e7bae8965cc387a5f153050c6737
divyansh2606 <111186936+divyansh2606@users.noreply.github.com>
2022-08-21 15:57:30 +0530

    Add files via upload

---
+++ enter time.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{   int hour,minute;
+    printf("enter time in hh:mm format");
+    scanf("%d:%d",&hour,&minute);
+    printf("%d hour and %d minute",hour,minute);
+    return 0;
+}
